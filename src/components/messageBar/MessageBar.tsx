bc5f05cf2c07bd37035d42b8b2e68b1491a3092f
omer <oemer.peynirci@ontavio.de>
2022-12-01 15:39:21 +0100

    delete and delete all added

--- src/components/messageBar/MessageBar.tsx
+++ src/components/messageBar/MessageBar.tsx
@@ -1,9 +1,17 @@
 import styles from './messageBar.module.css';
+type MessageVariant="danger"|"success";
 
-const MessageBar = () => {
+type PropsMessageBar={
+    message:{
+        text:string,
+        variant:MessageVariant
+    }
+}
+const MessageBar = ({message}:PropsMessageBar) => {
+    const {text,variant}=message;
     return (
         <div>
-            <p className={styles.messageText + " " + styles.success}>Item Added To The List</p>
+            <p className={styles.messageText + " " + styles[variant]}>{text}</p>
         </div>
     );
 };
